171e4403203e77bebed794bf11c98a8cd55e364c
devkyoung2 <devkyoung2@gmail.com>
2024-07-17 16:20:07 +0900

    [자료구조] : TreeNode 구현

--- 자료구조/Node.ts
+++ 자료구조/Node.ts
@@ -1,10 +1,20 @@
 // ts에서 클래스는 타입으로 사용 가능.
 // 즉 아래의 Node는 클래스이자 타입임
 // export default는 dom의 Node와 중복되지 않기 위해 모듈화
-export default class Node {
+export class Node {
   value: number;
 
   constructor(value: number) {
     this.value = value;
+  }
+}
+
+export class TreeNode extends Node {
+  children: TreeNode[];
+  parent?: TreeNode;
+
+  constructor(value: number) {
+    super(value);
+    this.children = [];
   }
 }
