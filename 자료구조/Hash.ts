1ad08b9eb637c90f0ac931186acedc13b9adfe09
devkyoung2 <devkyoung2@gmail.com>
2024-07-17 18:05:10 +0900

    [자료구조] : 해시 구현

---
+++ 자료구조/Hash.ts
@@ -0,0 +1,37 @@
+class HashTable<K, V> {
+  private table: Map<K, V>[];
+
+  constructor(private size: number) {
+    this.table = new Array(size).fill(new Map<K, V>());
+  }
+
+  private hash(key: K): number {
+    let hashValue = 0;
+
+    if (typeof key === 'string') {
+      for (let i = 0; i < key.length; i++) {
+        hashValue += key.charCodeAt(i);
+      }
+    } else if (typeof key === 'number') {
+      hashValue = key;
+    }
+
+    return hashValue % this.size;
+  }
+
+  push(key: K, value: V): void {
+    const index = this.hash(key);
+    this.table[index].set(key, value);
+  }
+
+  get(key: K): V | undefined {
+    const index = this.hash(key);
+
+    return this.table[index].get(key);
+  }
+
+  remove(key: K): void {
+    const index = this.hash(key);
+    this.table[index].delete(key);
+  }
+}
